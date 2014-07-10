a194c7376837f2ceb898f3a5bc7b47c0e0c4a775
AllanLArroque <allan.larroque@orange.fr>
2014-07-10 17:04:17 +0200

    maj

--- gen/com/example/application_bicloo/R.java
+++ gen/com/example/application_bicloo/R.java
@@ -13,8 +13,14 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int listViewArret=0x7f060000;
+        public static final int name=0x7f060001;
+        public static final int place=0x7f060002;
+    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int bikes_station_row=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- src/com/example/application_bicloo/MainActivity.java
+++ src/com/example/application_bicloo/MainActivity.java
@@ -1,15 +1,93 @@
 package com.example.application_bicloo;
 
+import java.util.ArrayList;
+
+import org.apache.http.protocol.ResponseServer;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	private ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        final ArrayList<String> list = new ArrayList<String>();
+        
+        // CREATION DE L'ADAPTER
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        
+        // RECUPERATION DE LA LISTVIEW
+        ListView listView = (ListView) findViewById(R.id.listViewArret);
+        listView.setAdapter(arrayAdapter);
+        
+        // REQUETE
+        RequestQueue queue = Volley.newRequestQueue(this);
+        
+        // REQUETE DE L'API AVEC LES PARAMETRES
+        String url = "https://api.jcdecaux.com/vls/v1/stations?contract=nantes&apiKey=d8cb851fb2471c0c4927e55405d96ded9ab1360f";
+        
+        JsonArrayRequest jsObjRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+
+			@Override
+			public void onResponse(JSONArray response) {
+				
+			// BOUCLE D'AJOUT DES INFORMATIONS
+				for (int i=0; i<response.length(); i++){
+					
+					try {
+						
+						// RECUPERATION DE L'OBJET ARRET
+						JSONObject arret = response.getJSONObject(i);
+						
+						// RECUPERATION DES ATTRIBUTS DE L'OBJET
+						String bikeStation = arret.getString("name");
+						int availableBikeStands = arret.getInt("available_bike_stands");
+						int bikeStands  = arret.getInt("bike_stands");
+						
+						//AJOUT DU NOM DE L'ARRET
+						arrayAdapter.add(bikeStation);
+						
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}		
+				
+				// RAFRAICHISSEMENT DE LA LISTVIEW
+				arrayAdapter.setNotifyOnChange(true);
+														}
+																										}, new Response.ErrorListener() {
+
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				// MESSAGE EN CAS D'ERREUR DE NON RETOUR
+				Log.d("ERROR API, REQUEST BAD", error.toString());
+			}
+		});
+        
+        queue.add(jsObjRequest);
+        
+        
+        
+        
+        
     }
 }
